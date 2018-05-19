82a625b4a4dfc3caa978eb15da1ca476989482ad
Matthew Lewis <mel_long@live.com>
2018-05-19 02:54:26 +0100

    Added comments to Base64 example tile icons in BitMask4Tile. Sliced full placeholder tileset and used sprites to replace current sprites in SandTerrainArea.asset

--- UnityTilemapTileBitmask/Assets/Scripts/Tiles/BM_TerrainAreaTile.cs
+++ UnityTilemapTileBitmask/Assets/Scripts/Tiles/BM_TerrainAreaTile.cs
@@ -104,20 +104,7 @@ namespace UnityEngine.Tilemaps {
     internal class BM_TerrainAreaTileEditor:Editor {
         private BM_TerrainAreaTile tile { get { return (target as BM_TerrainAreaTile); } }
         private const int k_bitSpriteCount = 10;
-
-        // Binary directonal grid component settings.
-        private int m_bitMaskTotal = 0;
-        private string m_binDirString = "// ";
-        private int[] m_binDirSelectedStates = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        private string[] m_binDirDirections = new string[] { "nw", "n", "ne", "w", "", "e", "sw", "s", "se" };
-        private static readonly int[] k_bitMaskValues = { 1, 2, 4, 8, 0, 16, 32, 64, 128 };
         private const float k_spriteWH = 16f * 4f;
-
-        // Colors
-        private static readonly Color k_binDirButtonOffColor = new Color(0.5f, 0.7f, 1f, 0.5f);
-        private static readonly Color k_binDirButtonOnColor = new Color(0.5f, 0.7f, 1f, 1f);
-        private static readonly Color k_textWhite = new Color(1f,1f,1f);
-        private static readonly Color k_cellTextDarkBlue = new Color(0f, 0.2f, 0.5f, 0.5f);
 
         // Tile Example Sprite Icons
         private const string s_spriteIcon0 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAFklEQVQI12MAgvp/IJTAhgdB1ADVAgDvdAnxdKVuwAAAAABJRU5ErkJggg==";
@@ -175,8 +162,6 @@ namespace UnityEngine.Tilemaps {
 
             EditorGUILayout.Space();
 
-            createBitMaskCalculator();
-
             // Draw actual tile sprite fields. These are used to assign the correct sprite to the tile when placing
             for (int i = 0; i < k_bitSpriteCount; i++) {
                 Rect r = (Rect)EditorGUILayout.BeginVertical();
@@ -190,134 +175,11 @@ namespace UnityEngine.Tilemaps {
             EditorGUI.EndChangeCheck();
         }
 
-        private void createBitMaskCalculator() {
-
-            // Binary directional settings
-            Rect binDirRect = new Rect(EditorGUIUtility.currentViewWidth - (k_spriteWH * 1.25f) - 20f, 118f, k_spriteWH*1.25f, k_spriteWH*1.25f);
-            Handles.color = EditorGUIUtility.isProSkin ? new Color(1f, 1f, 1f, 0.2f) : new Color(0f, 0f, 0f, 0.2f);
-            // Binary directional grid cell width/height
-            float binDirWH = binDirRect.width / 3f;
-
-            GUIStyle BitMaskValuesStyle = new GUIStyle();
-            BitMaskValuesStyle.alignment = TextAnchor.MiddleCenter;
-            BitMaskValuesStyle.normal.textColor = k_textWhite;
-            BitMaskValuesStyle.normal.background = MakeTex((int)binDirWH, (int)binDirWH, k_binDirButtonOffColor);
-
-            GUIStyle BitMaskTotalStyle = new GUIStyle();
-            BitMaskTotalStyle.alignment = TextAnchor.MiddleCenter;
-            BitMaskTotalStyle.fontSize = 20;
-
-            GUIStyle BinDirDirectionStringStyle = new GUIStyle();
-            BinDirDirectionStringStyle.alignment = TextAnchor.MiddleLeft;
-            BinDirDirectionStringStyle.normal.textColor = new Color(0f,0.5f,0f);
-            BinDirDirectionStringStyle.fontSize = 10;
-
-            GUIStyle BinDirGridDirectionsStyle = new GUIStyle();
-            BinDirGridDirectionsStyle.alignment = TextAnchor.UpperCenter;
-            BinDirGridDirectionsStyle.normal.textColor = k_cellTextDarkBlue;
-            BinDirGridDirectionsStyle.fontSize = 8;
-
-            m_binDirString = "// ";
-
-            // Draw Binary directional grid
-            for (int y = 0; y <=3; y++) {
-                float top = binDirRect.yMin + y * binDirWH;
-                Handles.DrawLine(new Vector3(binDirRect.xMin, top), new Vector3(binDirRect.xMax, top));
-            }
-            for (int x = 0; x <= 3; x++) {
-                float left = binDirRect.xMin + x * binDirWH;
-                Handles.DrawLine(new Vector3(left, binDirRect.yMin), new Vector3(left, binDirRect.yMax));
-            }
-            Handles.color = Color.white;
-
-            int binDirCellIndex = 0;
-            for (int y = 0; y <= 2; y++) {
-                for (int x = 0; x <= 2; x++) {
-
-                    Rect r = new Rect(binDirRect.x + (binDirWH * x) - 1, binDirRect.y + (binDirWH * y) - 1, binDirWH + 1, binDirWH + 1);
-
-                    if (x != 1 || y != 1) {
-                        if (Event.current.type == EventType.MouseDown && r.Contains(Event.current.mousePosition) && Event.current.button == 0) {
-
-                            Event.current.Use();
-
-                            // Set selected states for binary directional cells
-                            if (m_binDirSelectedStates[binDirCellIndex] == 0) {
-                                m_binDirSelectedStates[binDirCellIndex] = 1;
-                            } else {
-                                m_binDirSelectedStates[binDirCellIndex] = 0;
-                            }
-
-                            sumBitMaskValues(binDirCellIndex);
-                        }
-
-                        // Set background color of binary directional cells
-                        if (m_binDirSelectedStates[binDirCellIndex] == 0) {
-                            BitMaskValuesStyle.normal.background = MakeTex((int)binDirWH, (int)binDirWH, k_binDirButtonOffColor);
-                        } else {
-                            BitMaskValuesStyle.normal.background = MakeTex((int)binDirWH, (int)binDirWH, k_binDirButtonOnColor);
-                        }
-
-                        GUI.Box(r, k_bitMaskValues[binDirCellIndex].ToString(), BitMaskValuesStyle);
-
-                        // Set correct direction text alignment
-                        if(y == 0 && x == 0) { BinDirGridDirectionsStyle.alignment = TextAnchor.UpperLeft; }
-                        else if(y == 0 && x == 1){ BinDirGridDirectionsStyle.alignment = TextAnchor.UpperCenter; }
-                        else if(y == 0 && x == 2){ BinDirGridDirectionsStyle.alignment = TextAnchor.UpperRight; }
-                        else if (y == 1 && x == 0) { BinDirGridDirectionsStyle.alignment = TextAnchor.MiddleLeft; }
-                        else if (y == 1 && x == 2) { BinDirGridDirectionsStyle.alignment = TextAnchor.MiddleRight; }
-                        else if (y == 2 && x == 0) { BinDirGridDirectionsStyle.alignment = TextAnchor.LowerLeft; }
-                        else if (y == 2 && x == 1) { BinDirGridDirectionsStyle.alignment = TextAnchor.LowerCenter; }
-                        else if (y == 2 && x == 2) { BinDirGridDirectionsStyle.alignment = TextAnchor.LowerRight; }
-
-                        GUI.Box(r, m_binDirDirections[binDirCellIndex], BinDirGridDirectionsStyle);
-                    }
-
-                    binDirCellIndex++;
-                }
-            }
-
-            for(int i = 0; i < m_binDirSelectedStates.Length; i++) {
-                if (m_binDirSelectedStates[i] == 1) {
-                    m_binDirString += m_binDirDirections[i] + " ";
-                }
-            }
-
-            // Selected direction string as code comment for quicker pasting next to your BitMask values in code for referencing later in custom tile scripts.
-            if (m_binDirString != "// ") {
-                EditorGUI.TextArea(new Rect(binDirRect.x - (binDirRect.width * 1.9f), binDirRect.y, binDirRect.width, binDirRect.height / 3), m_binDirString, BinDirDirectionStringStyle);
-            }
-
-            // Bit Mask Value
-            EditorGUI.TextArea(new Rect(binDirRect.x - (binDirRect.width * 1.625f), binDirRect.y, binDirRect.width, binDirRect.height), m_bitMaskTotal.ToString(), BitMaskTotalStyle);
-
-            // Lazy way to get spacing. Might mess up on different size screens
-            for (int i = 0; i < 14; i++) { EditorGUILayout.Space(); }
-        }
-
         private static Texture2D Base64ToTexture(string base64) {
             Texture2D t = new Texture2D(1, 1);
             t.hideFlags = HideFlags.HideAndDontSave;
             t.LoadImage(System.Convert.FromBase64String(base64));
             return t;
-        }
-
-        private Texture2D MakeTex(int width, int height, Color col) {
-            // Used for GUI.Box background colors
-            Color[] pix = new Color[width * height];
-            for (int i = 0; i < pix.Length; ++i) {
-                pix[i] = col;
-            }
-            Texture2D result = new Texture2D(width-1, height-1);
-            result.SetPixels(pix);
-            result.Apply();
-            return result;
-        }
-
-        private void sumBitMaskValues(int binDirCellIndex) {
-
-            // pow2 value * binaryValue (-1 if 0 to subtract pow2 value from BitMaskTotal)
-            m_bitMaskTotal += k_bitMaskValues[binDirCellIndex] * (m_binDirSelectedStates[binDirCellIndex] == 0 ? -1 : m_binDirSelectedStates[binDirCellIndex]);
         }
     }
 #endif
