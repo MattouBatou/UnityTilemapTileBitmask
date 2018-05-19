cf9b31d31bf51a48a5b1cdc702f82660989443d0
Matthew Lewis <mel_long@live.com>
2018-05-19 02:18:17 +0100

    Created script for "Terrain Area Tile" tile type and removed redundent code from it.

--- UnityTilemapTileBitmask/Assets/Scripts/Tiles/BM_TerrainAreaTile.cs
+++ UnityTilemapTileBitmask/Assets/Scripts/Tiles/BM_TerrainAreaTile.cs
@@ -10,7 +10,7 @@ using UnityEditor;
 namespace UnityEngine.Tilemaps {
 
     [Serializable]
-    public class BitMask4Tile:TileBase {
+    public class BM_TerrainAreaTile:TileBase {
 
         [SerializeField]
         public Sprite[] m_BitSprites;
@@ -25,7 +25,6 @@ namespace UnityEngine.Tilemaps {
         }
 
         public override void RefreshTile(Vector3Int location, ITilemap tilemap) {
-            //base.RefreshTile(location, tilemap);
             checkNeighboringTiles(location, tilemap);
         }
 
@@ -62,10 +61,6 @@ namespace UnityEngine.Tilemaps {
 
             int bitMaskValue = NeighborsToTileIndexTerrain(mask);
 
-            //if(binDirCellIndex == 0) {
-            //    binDirCellIndex = NeighborsToTileIndexPipe(mask);
-            //}
-
             if (bitMaskValue >= 0 && bitMaskValue < m_BitSprites.Length && NeighboringTileAtPos(tilemap, location)) {
 
                 tileData.sprite = m_BitSprites[bitMaskValue];
@@ -77,70 +72,38 @@ namespace UnityEngine.Tilemaps {
 
             switch (mask) {
 
-                case 11: return 3;      // nw n w
-
-                case 22: return 5;      // n ne e
-
-                case 31: return 7;      // nw n ne w e
-
-                case 208: return 12;    // e s se
-
-                case 214: return 13;    // n ne e s se
-
-                case 248: return 14;    // w e sw s se
-
-                case 104: return 10;    // w sw s
-
-                case 107: return 11;    // nw n w sw s
-
-                case 255: return 15;    // all
-            }
-            return 0;
-        }
-
-        private int NeighborsToTileIndexPipe(int mask) {
-            // Cases grouped by sprite selection.
-
-            switch (mask) {
-
-                case 2: return 1;       // n
-
-                case 16: return 2;      // e
-
-                case 10: return 3;      // w s
-
-                case 8: return 4;       // w
-
-                case 18: return 5;      // n e
-
-                case 64: return 8;      // s
-
-                case 72: return 10;     // w s
-
-                case 80: return 12;     // e s
+                case 11: return 1;      // nw n w
+                case 22: return 2;      // n ne e
+                case 31: return 3;      // nw n ne w e
+                case 104: return 4;    // w sw s
+                case 107: return 5;    // nw n w sw s
+                case 208: return 6;    // e s se
+                case 214: return 7;    // n ne e s se
+                case 248: return 8;    // w e sw s se
+                case 255: return 9;    // all
             }
             return 0;
         }
 
 #if UNITY_EDITOR
-        [MenuItem("Assets/Create/BitMask4Tile")]
-        public static void CreateBitMask4Tile() {
-            string path = EditorUtility.SaveFilePanelInProject("Save BitMask 4-bit Tile", "New 4-bit Bitmask Tile", "asset", "Save BitMask 4-bit Tile", "Assets");
+        [MenuItem("Assets/Create/BM_TerrainAreaTile")]
+        public static void CreateBM_TerrainAreaTile() {
+            string path = EditorUtility.SaveFilePanelInProject("Save BitMask Terrain Area Tile", "New 4-bit Bitmask Tile", "asset", "Save BitMask Terrain Area Tile", "Assets");
             if (path == "") {
                 return;
             }
 
-            AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<BitMask4Tile>(), path);
+            AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<BM_TerrainAreaTile>(), path);
         }
 #endif
     }
 
 #if UNITY_EDITOR
-    [CustomEditor(typeof(BitMask4Tile))]
+    [CustomEditor(typeof(BM_TerrainAreaTile))]
     [CanEditMultipleObjects]
-    internal class BitMask4TileEditor:Editor {
-        private BitMask4Tile tile { get { return (target as BitMask4Tile); } }
-        private const int k_bitSpriteCount = 16;
+    internal class BM_TerrainAreaTileEditor:Editor {
+        private BM_TerrainAreaTile tile { get { return (target as BM_TerrainAreaTile); } }
+        private const int k_bitSpriteCount = 10;
 
         // Binary directonal grid component settings.
         private int m_bitMaskTotal = 0;
@@ -158,21 +121,15 @@ namespace UnityEngine.Tilemaps {
 
         // Tile Example Sprite Icons
         private const string s_spriteIcon0 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAFklEQVQI12MAgvp/IJTAhgdB1ADVAgDvdAnxdKVuwAAAAABJRU5ErkJggg==";
-        private const string s_spriteIcon1 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAFElEQVQI12MAggQ2wqj+HxAB1QIAlMEHw4qUPRAAAAAASUVORK5CYII=";
-        private const string s_spriteIcon2 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAFUlEQVQI12MAgvp/IMTAhgfB1DAAAJOUBjEmAwzVAAAAAElFTkSuQmCC";
-        private const string s_spriteIcon3 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAE0lEQVQI12MAATbCqP4fEAGVAgAZAQND5NXRlgAAAABJRU5ErkJggg==";
-        private const string s_spriteIcon4 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAFklEQVQI12MAgvp/IJTAgBtB1QDVAgDp8gm1mW00CAAAAABJRU5ErkJggg==";
-        private const string s_spriteIcon5 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAE0lEQVQI12MAggQiUP0/IAKqBQCNTQd7L2yUwAAAAABJRU5ErkJggg==";
-        private const string s_spriteIcon6 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAEklEQVQI12MAgvp/IIQPINQAAI4SBfW8z+asAAAAAElFTkSuQmCC";
-        private const string s_spriteIcon7 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAEElEQVQI12MgEtT/AyIgDQARjQL7N/HyZAAAAABJRU5ErkJggg==";
-        private const string s_spriteIcon8 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAFElEQVQI12MAgvp/IJTARhAB1QIA4zkHw20XQZEAAAAASUVORK5CYII=";
-        private const string s_spriteIcon9 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAEElEQVQI12MAggQ2IhFQLQCIhgWVP/96aQAAAABJRU5ErkJggg==";
-        private const string s_spriteIcon10 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAE0lEQVQI12MAgvp/IMTARgRiAACCeQNDJCF45wAAAABJRU5ErkJggg==";
-        private const string s_spriteIcon11 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAADklEQVQI12MAATaiEQMAB+YAVQErUaIAAAAASUVORK5CYII=";
-        private const string s_spriteIcon12 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAE0lEQVQI12MAgvp/IJTAQBABAQDddQd7cGz9TwAAAABJRU5ErkJggg==";
-        private const string s_spriteIcon13 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAD0lEQVQI12MAggRiERAAAIDQBUEcq+81AAAAAElFTkSuQmCC";
-        private const string s_spriteIcon14 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAEElEQVQI12MAgvp/IEQcAAB8tQL7Ry7GCQAAAABJRU5ErkJggg==";
-        private const string s_spriteIcon15 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA1BMVEUB/wA1nKqfAAAAC0lEQVQI12MgEQAAADAAAWV61nwAAAAASUVORK5CYII=";
+        private const string s_spriteIcon1 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAE0lEQVQI12MAATbCqP4fEAGVAgAZAQND5NXRlgAAAABJRU5ErkJggg==";
+        private const string s_spriteIcon2 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAE0lEQVQI12MAggQiUP0/IAKqBQCNTQd7L2yUwAAAAABJRU5ErkJggg==";
+        private const string s_spriteIcon3 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAEElEQVQI12MgEtT/AyIgDQARjQL7N/HyZAAAAABJRU5ErkJggg==";
+        private const string s_spriteIcon4 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAE0lEQVQI12MAgvp/IMTARgRiAACCeQNDJCF45wAAAABJRU5ErkJggg==";
+        private const string s_spriteIcon5 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAADklEQVQI12MAATaiEQMAB+YAVQErUaIAAAAASUVORK5CYII=";
+        private const string s_spriteIcon6 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAE0lEQVQI12MAgvp/IJTAQBABAQDddQd7cGz9TwAAAABJRU5ErkJggg==";
+        private const string s_spriteIcon7 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAD0lEQVQI12MAggRiERAAAIDQBUEcq+81AAAAAElFTkSuQmCC";
+        private const string s_spriteIcon8 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUB/wAAAACkX63mAAAAEElEQVQI12MAgvp/IEQcAAB8tQL7Ry7GCQAAAABJRU5ErkJggg==";
+        private const string s_spriteIcon9 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA1BMVEUB/wA1nKqfAAAAC0lEQVQI12MgEQAAADAAAWV61nwAAAAASUVORK5CYII=";
 
         private static Texture2D[] s_spriteIcons;
         private static Texture2D[] spriteIcons {
@@ -189,12 +146,6 @@ namespace UnityEngine.Tilemaps {
                     s_spriteIcons[7] = Base64ToTexture(s_spriteIcon7);
                     s_spriteIcons[8] = Base64ToTexture(s_spriteIcon8);
                     s_spriteIcons[9] = Base64ToTexture(s_spriteIcon9);
-                    s_spriteIcons[10] = Base64ToTexture(s_spriteIcon10);
-                    s_spriteIcons[11] = Base64ToTexture(s_spriteIcon11);
-                    s_spriteIcons[12] = Base64ToTexture(s_spriteIcon12);
-                    s_spriteIcons[13] = Base64ToTexture(s_spriteIcon13);
-                    s_spriteIcons[14] = Base64ToTexture(s_spriteIcon14);
-                    s_spriteIcons[15] = Base64ToTexture(s_spriteIcon15);
                 }
                 return s_spriteIcons;
             }
